470254bd001e3eeee6e93ac1994c389582d52af5
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-12-02 21:05:38 +0000

    add x y axis

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,65 +1,30 @@
-"use client";
-import { useMemo } from "react";
-import { useChartData } from "./useChart";
-
-import * as d3 from "d3";
-
-const Axis = ({ domain = [0, 100], range = [10, 700] }) => {
-  const ticks = useMemo(() => {
-    const xScale = d3.scaleLinear().domain(domain).range(range);
-
-    const width = range[1] - range[0];
-    const pixelsPerTick = 30;
-    const numberOfTicksTarget = Math.max(1, Math.floor(width / pixelsPerTick));
-
-    return xScale.ticks(numberOfTicksTarget).map((value) => ({
-      value,
-      xOffset: xScale(value),
-    }));
-  }, [domain.join("-"), range.join("-")]);
-
-  return (
-    <g>
-      <path
-        d={["M", range[0], 6, "v", -6, "H", range[1], "v", 6].join(" ")}
-        fill="none"
-        stroke="currentColor"
-      />
-      {ticks.map(({ value, xOffset }) => (
-        <g key={value} transform={`translate(${xOffset}, 0)`}>
-          <line y2="6" stroke="currentColor" />
-          <text
-            key={value}
-            style={{
-              fontSize: "10px",
-              textAnchor: "middle",
-              transform: "translateY(20px)",
-            }}
-          >
-            {value}
-          </text>
-        </g>
-      ))}
-    </g>
-  );
-};
+"use client"
+import { useChartData } from "./useChart"
+import { XAxis, YAxis } from "@/components/Axis"
 
 export default function Page() {
-  const { data } = useChartData({ symbol: "SPY" });
+  const { data } = useChartData({ symbol: "SPY" })
 
-  const width = 800;
-  const height = 400;
-  const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-  
+  const width = 440
+  const height = 440
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 }
 
   return (
     <svg width={width} height={height}>
       <g>
-    
-         <g transform={`translate(0, ${height - margin.bottom})`}>
-          <Axis domain={[0, 100]} range={[margin.left, width - margin.right]} />
+        <g transform={`translate(${width-margin.left}, 0)`}>
+          <YAxis
+            domain={[100, 0]}
+            range={[margin.top, height - margin.bottom]}
+          />
+        </g>
+        <g transform={`translate(0, ${height - margin.bottom})`}>
+          <XAxis
+            domain={[0, 100]}
+            range={[margin.left, width - margin.right]}
+          />
         </g>
       </g>
     </svg>
-  );
+  )
 }
---
+++ components/Axis.tsx
@@ -0,0 +1,84 @@
+import * as d3 from "d3"
+import { useMemo } from "react"
+
+export const XAxis = ({ domain = [0, 100], range = [10, 700] }) => {
+  const ticks = useMemo(() => {
+    const xScale = d3.scaleLinear().domain(domain).range(range)
+
+    const width = range[1] - range[0]
+    const pixelsPerTick = 30
+    const numberOfTicksTarget = Math.max(1, Math.floor(width / pixelsPerTick))
+
+    return xScale.ticks(numberOfTicksTarget).map((value) => ({
+      value,
+      xOffset: xScale(value),
+    }))
+  }, [domain.join("-"), range.join("-")])
+
+  return (
+    <g>
+      <path
+        d={["M", range[0], 6, "v", -6, "H", range[1], "v", 6].join(" ")}
+        fill="none"
+        stroke="currentColor"
+      />
+      {ticks.map(({ value, xOffset }) => (
+        <g key={value} transform={`translate(${xOffset}, 0)`}>
+          <line y2="6" stroke="currentColor" />
+          <text
+            key={value}
+            style={{
+              fontSize: "10px",
+              textAnchor: "middle",
+              transform: "translateY(20px)",
+            }}
+          >
+            {value}
+          </text>
+        </g>
+      ))}
+    </g>
+  )
+}
+
+
+
+export const YAxis = ({ domain = [0, 100], range = [10, 300] }) => {
+  const ticks = useMemo(() => {
+    const yScale = d3.scaleLinear().domain(domain).range(range)
+
+    const width = range[1] - range[0]
+    const pixelsPerTick = 30
+    const numberOfTicksTarget = Math.max(1, Math.floor(width / pixelsPerTick))
+
+    return yScale.ticks(numberOfTicksTarget).map((value) => ({
+      value,
+      yOffset: yScale(value),
+    }))
+  }, [domain.join("-"), range.join("-")])
+
+  return (
+    <g>
+      <path
+        d={["M", 6, range[0], "h", -6, "V", range[1], "h", 6].join(" ")}
+        fill="none"
+        stroke="currentColor"
+      />
+      {ticks.map(({ value, yOffset }) => (
+        <g key={value} transform={`translate(0, ${yOffset})`}>
+          <line x2="6" stroke="currentColor" />
+          <text
+            key={value}
+            style={{
+              fontSize: "10px",
+              textAnchor: "middle",
+              transform: "translateX(10px)",
+            }}
+          >
+            {value}
+          </text>
+        </g>
+      ))}
+    </g>
+  )
+}
