51e082b82c6654535e2e95ca94ccb1db225f5e2e
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-12-03 16:19:44 +0000

    Update chart settings and scales

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -8,12 +8,14 @@ import { XAxis, YAxis } from "@/components/Axis"
 export default function Page() {
   // const { data } = useChartData({ symbol: "SPY" })
   const { height, width } = useWindowSize()
+
   const chartSettings = {
-    width: (width || 460) * 0.8,
-    height: (height || 460) * 0.8,
-    margin: { top: 20, right: 40, bottom: 40, left: 20 },
+    width: width || 800,
+    height: height || 600,
+    margin: { top: 40, right: 80, bottom: 80, left: 40 },
   }
   const { ref, dms } = useChartDimensions(chartSettings)
+  
   const xScale = useMemo(
     () =>
       d3
@@ -22,17 +24,25 @@ export default function Page() {
         .range([dms.marginLeft, dms.marginLeft + dms.boundedWidth]),
     [dms.boundedWidth],
   )
+  const yScale = useMemo(
+    () =>
+      d3
+        .scaleLinear()
+        .domain([100, 0])
+        .range([dms.marginTop, dms.marginTop + dms.boundedHeight]),
+    [dms.boundedHeight],
+  )
 
   return (
     <div ref={ref}>
       <svg width={dms.width} height={dms.height}>
         <g transform={`translate(${dms.marginLeft + dms.boundedWidth}, 0)`}>
           <YAxis
-            domain={[100, 0]}
-            range={[dms.marginTop, dms.height - dms.marginBottom]}
+            domain={yScale.domain()}
+            range={yScale.range()}
           />
         </g>
-        <g transform={`translate(0, ${dms.height - dms.marginBottom})`}>
+        <g transform={`translate(0, ${dms.marginTop + dms.boundedHeight})`}>
           <XAxis domain={xScale.domain()} range={xScale.range()} />
         </g>
       </svg>
