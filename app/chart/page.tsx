9bb9a5a4dcec1b20eb7e637a41e00395eae5f65f
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-12-03 16:12:25 +0000

    make chart width responsive

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,15 +1,16 @@
 "use client"
 import * as d3 from "d3"
 import { useMemo } from "react"
+import { useWindowSize } from "@uidotdev/usehooks";
 import { useChartDimensions } from "./useChartDimensions"
 import { XAxis, YAxis } from "@/components/Axis"
 
 export default function Page() {
   // const { data } = useChartData({ symbol: "SPY" })
-
+  const { height, width } = useWindowSize()
   const chartSettings = {
-    width: 460,
-    height: 460,
+    width: (width || 460) * 0.8,
+    height: (height || 460) * 0.8,
     margin: { top: 20, right: 40, bottom: 40, left: 20 },
   }
   const { ref, dms } = useChartDimensions(chartSettings)
@@ -18,14 +19,14 @@ export default function Page() {
       d3
         .scaleLinear()
         .domain([0, 100])
-        .range([dms.marginLeft, dms.boundedWidth]),
+        .range([dms.marginLeft, dms.marginLeft + dms.boundedWidth]),
     [dms.boundedWidth],
   )
 
   return (
     <div ref={ref}>
       <svg width={dms.width} height={dms.height}>
-        <g transform={`translate(${dms.boundedWidth}, 0)`}>
+        <g transform={`translate(${dms.marginLeft + dms.boundedWidth}, 0)`}>
           <YAxis
             domain={[100, 0]}
             range={[dms.marginTop, dms.height - dms.marginBottom]}
